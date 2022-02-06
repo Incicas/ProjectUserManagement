88c38c726376e49451c728fa0607a6a869916139
Incicas Beniamin <incicas.beniamin@gmail.com>
2022-02-06 18:30:44 +0200

    Creating project

---
+++ src/main/java/com/example/ProjectUserManagement/ProjectUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUserManagementApplication.class, args);
+	}
+
+}
